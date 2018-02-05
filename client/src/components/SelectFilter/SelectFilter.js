47138dda61d572efc4c25dcf8b830e595be7ed60
Scott <scottdlivingstone@gmail.com>
2018-02-04 20:44:17 -0800

    Remove selection rendering from select filter as it was not working with the tag ids

--- client/src/components/SelectFilter/SelectFilter.js
+++ client/src/components/SelectFilter/SelectFilter.js
@@ -7,65 +7,34 @@ import gql from 'graphql-tag';
 
 import { filterItems } from '../../redux/modules/items';
 
-// const menuItems = [
-//     {
-//         value: 1,
-//         primaryText: 'Household Items'
-//     },
-//     {
-//         value: 2,
-//         primaryText: 'Recreational Equipment'
-//     },
-//     {
-//         value: 3,
-//         primaryText: 'Electronics'
-//     },
-//     {
-//         value: 4,
-//         primaryText: 'Physical Media'
-//     },
-//     {
-//         value: 5,
-//         primaryText: 'Musical Instruments'
-//     }
-// ];
 class SelectFilter extends React.Component {
     handleChange = (event, index, selected) => {
         this.props.dispatch(filterItems(selected));
     };
 
-    selectionRenderer = selected => {
-        switch (selected.length) {
-        case 0:
-            return '';
-        case 1:
-            return selected;
-        default:
-            return `${selected.length} tags selected`;
-        }
-    };
-
     menuItemGenerator(items) {
-        return items.map(item => (
-            <MenuItem
-                checked={this.props.tags.indexOf(item.id) > -1}
-                insetChildren
-                key={item.id}
-                value={item.id}
-                primaryText={item.title}
-            />
-        ));
+        return items.map(item => {
+            const tagId = item.id;
+            const tagTitle = item.title;
+            return (
+                <MenuItem
+                    checked={this.props.tags.indexOf(item.id) > -1}
+                    insetChildren
+                    key={item.id}
+                    value={item.id}
+                    primaryText={item.title}
+                />
+            );
+        });
     }
 
     render() {
         const { tags } = this.props.data;
-        console.log(tags);
         return (
             <SelectField
                 value={this.props.tags}
                 onChange={this.handleChange}
                 multiple
-                selectionRenderer={this.selectionRenderer}
                 floatingLabelText="Filter by tag"
             >
                 {this.menuItemGenerator(tags || [])}
